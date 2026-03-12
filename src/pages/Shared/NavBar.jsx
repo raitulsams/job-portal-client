36da118165682d5c5d728abff06987af96a1be7e
raitulsams <raitulsams202@gmail.com>
2026-03-12 02:40:41 +0100

    logo added

--- src/pages/Shared/NavBar.jsx
+++ src/pages/Shared/NavBar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router";
-
+import logo from "../../assets/logo.png"
 const NavBar = () => {
     const links = (
         <>
@@ -25,13 +25,28 @@ const NavBar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
+
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <NavLink to="/">
+
+
+                    <div className="relative z-10 flex justify-center items-center h-full">
+                        <img
+                            src={logo}
+                            alt="Espresso Emporium Logo"
+                            className="h-12 w-auto object-contain"
+                        />
+                        <p className="font-rancho text-xl">
+                            Arbeit
+                        </p>
+                    </div>
+                </NavLink>
+                {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
