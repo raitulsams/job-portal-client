88a892d3f62aa2140c7db4b74fa464528aabc41b
raitulsams <raitulsams202@gmail.com>
2026-03-12 02:16:14 +0100

    securing firebaseConfig

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -7,10 +7,8 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: import.meta.env.apiKey,
-
     authDomain: import.meta.env.authDomain,
     projectId: import.meta.env.projectId,
-
     storageBucket: import.meta.env.storageBucket,
     messagingSenderId: import.meta.env.messagingSenderId,
     appId: import.meta.env.appId
